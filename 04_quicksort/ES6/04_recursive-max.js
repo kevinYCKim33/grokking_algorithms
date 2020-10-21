97e1167daf88b87a43227afaf4f1af5255f9ecf3
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-10-20 21:21:34 -0400

    do recursive max my own way

--- 04_quicksort/ES6/04_recursive-max.js
+++ 04_quicksort/ES6/04_recursive-max.js
@@ -7,3 +7,18 @@ const max = (list) => {
 };
 
 console.log(max([1, 5, 10, 25, 16, 1])); // 25
+
+// find max num in a list
+
+const kevinFindMax = (list, king = list[0]) => {
+  // base case
+  // empty array
+  if (list.length === 0) return king;
+
+  // divide and conquer
+  const biggerKing = king > list[0] ? king : list[0];
+
+  return kevinFindMax(list.slice(1), biggerKing);
+};
+
+console.log(kevinFindMax([8, 6, 7, 5, 3, 18, 9])); // 18
--- sandbox/index.js
+++ sandbox/index.js
@@ -1,8 +1,14 @@
-const count = (list) => {
-  debugger;
-  if (list.length === 0) return 0;
+// find max num in a list
 
-  return 1 + count(list.slice(1));
+const findMax = (list, king = list[0]) => {
+  // base case
+  // empty array
+  if (list.length === 0) return king;
+
+  // divide and conquer
+  const biggerKing = king > list[0] ? king : list[0];
+
+  return findMax(list.slice(1), biggerKing);
 };
 
-console.log(count([1, 2, 3, 4, 5, 6]));
+console.log(findMax([8, 6, 7, 5, 3, 18, 9])); // 18
